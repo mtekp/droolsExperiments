4e9224c7ddba59173a0b285f90942abcdf71a4ef
Mike <madamovych@gmail.com>
2020-11-13 18:10:44 +0200

    Converted to DroolsAssert test

--- testdrools/src/test/java/com/testing/drools/test/events/continuous/TestDroolEventsContinousTest.java
+++ testdrools/src/test/java/com/testing/drools/test/events/continuous/TestDroolEventsContinousTest.java
@@ -1,49 +1,37 @@
 package com.testing.drools.test.events.continuous;
 
 import java.io.IOException;
-import java.time.Instant;
 import java.time.LocalDate;
-import java.util.Date;
 
+import org.droolsassert.DroolsAssert;
+import org.droolsassert.DroolsSession;
+import org.junit.Rule;
 import org.junit.Test;
 
-public class TestDroolEventsContinousTest {
-	SessionProxy sp;
-    Date startDate = new Date();
-
-    @Test
-    public void withInitialState() throws IOException {
-        sp = new TestDroolEventsContinous();
-
-        // insert EventDataMessage within one minute 6 times
-        Instant si = startDate.toInstant();
-
-
-
-        AnomalyEventInsert m1 = new AnomalyEventInsert(LocalDate.now());
-
-        sp.insert(m1);
-
-
-        AnomalyEventInsert m2 = new AnomalyEventInsert(LocalDate.now());
-        sp.insert(m2);
-
-
-        AnomalyEventInsert m3 = new AnomalyEventInsert(LocalDate.now());
-        sp.insert(m3);
-
-        AnomalyEventInsert m4 = new AnomalyEventInsert(LocalDate.now().plusDays(1));
-        sp.insert(m4);
-
-
-        AnomalyEventInsert m5 = new AnomalyEventInsert(LocalDate.now().plusDays(2));
-        sp.insert(m5);
-
-
-        AnomalyEventInsert m6 = new AnomalyEventInsert(LocalDate.now().plusDays(3));
-        sp.insert(m6);
-
-
-    }
-
+@DroolsSession("test/events/continuous/*.drl")
+public class TestDroolEventsContinousTest extends DroolsAssert {
+	
+	@Rule
+	public DroolsAssert droolsAssert = this;
+	
+	@Test
+	public void withInitialState() throws IOException {
+		AnomalyEventInsert m1 = new AnomalyEventInsert(LocalDate.now());
+		insertAndFire(m1);
+		
+		AnomalyEventInsert m2 = new AnomalyEventInsert(LocalDate.now());
+		insertAndFire(m2);
+		
+		AnomalyEventInsert m3 = new AnomalyEventInsert(LocalDate.now());
+		insertAndFire(m3);
+		
+		AnomalyEventInsert m4 = new AnomalyEventInsert(LocalDate.now().plusDays(1));
+		insertAndFire(m4);
+		
+		AnomalyEventInsert m5 = new AnomalyEventInsert(LocalDate.now().plusDays(2));
+		insertAndFire(m5);
+		
+		AnomalyEventInsert m6 = new AnomalyEventInsert(LocalDate.now().plusDays(3));
+		insertAndFire(m6);
+	}
 }
